2c1e1b2e206f05eb4f2435625a9538df6abf953d
Yahuza Mushe Abdul-Hakim <musheabdulhakim99@gmail.com>
2020-11-09 04:04:23 +0000

    scripts and static pages

---
+++ templates/includes/footer.php
@@ -0,0 +1,103 @@
+<footer id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="appointment-link">
+                            <div class="ap-inner">
+                                <div class="ap-icon">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                                </div>
+                                <div class="ap-text">
+                                    <h3>Need a Doctor for Check-up?</h3>
+                                    <h2>JUST MAKE AN APPOINTMENT & YOU'RE DONE ! </h2>
+                                </div>
+                            </div>
+                            <div class="ap-btn">
+                                <a href="#form" class="thm-btn page-scroll">get an appointment</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- Address -->
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-box address-inner">
+                            <img src="images/logo.png" class="img-responsive" alt="">
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                                been the.  </p>
+                            <div class="address">
+                                <i class="flaticon-placeholder"></i>
+                                <p>PO Box 16122 Collins Street<br>
+                                    West Victoria 8007 Australia</p>
+                            </div>
+                            <div class="address">
+                                <i class="flaticon-customer-service"></i>
+                                <p> +880153625892</p>
+                            </div>
+                            <div class="address">
+                                <i class="flaticon-mail"></i>
+                                <p><a href="#"><span class="__cf_email__" data-cfemail="d5b7b1a1b4a6be95b2b8b4bcb9fbb6bab8">[email&#160;protected]</span></a> </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Latest News -->
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-box">
+                            <h4 class="footer-title">Latest News</h4>
+                            <div class="post-widget">
+                                <ul>
+                                    <li>
+                                        <a href="#"><img src="images/footer-post-1.jpg"  alt="" ></a>
+                                        <a href="#">Healthy Lifestyle Tips</a>
+                                        <span class="post-date">Oct 6, 2016</span>
+                                    </li>
+                                    <li>
+                                        <a href="#"><img src="images/footer-post-2.jpg"  alt="" ></a>
+                                        <a href="#">Child Care</a>
+                                        <span class="post-date">Oct 6, 2016</span>
+                                    </li>
+                                    <li>
+                                        <a href="#"><img src="images/footer-post-3.jpg"  alt="" ></a>
+                                        <a href="#">Daily Exersise Is Essential</a>
+                                        <span class="post-date">Oct 6, 2016</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Latest Tweet -->
+                    <div class="col-md-3 col-sm-6">
+                        <div class="footer-box">
+                            <h4 class="footer-title">Latest Tweet</h4>
+                            <div class="latest-tweet">
+                                <ul>
+                                    <li>
+                                        <p class="tweet-text">Lorem Ipsum is <a  href="#" target="_blank">@simply</a> dummy text of the printing and <a  href="#" target="_blank">typesetting</a> industry.</p>
+                                        <p class="tweet-details"><a href="#" target="_blank"><span>7 minutes ago</span></a></p>
+                                    </li>
+                                    <li>
+                                        <p class="tweet-text">Lorem Ipsum is <a  href="#" target="_blank">@simply</a> dummy text of the printing and <a  href="#" target="_blank">typesetting</a> industry.</p>
+                                        <p class="tweet-details"><a href="#" target="_blank"><span>About 4 hours ago</span></a></p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Map -->
+                    <div class="col-md-3 col-sm-6">
+                        <div class="map">
+                            <h4 class="footer-title">Map</h4>
+                            <!-- The element that will contain our Google Map. This is used in both the Javascript and CSS above. -->
+                            <div id="map"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <div class="sub-footer">
+            <div class="container">
+                <div class="row">
+                    <p>Copyright © 2016. All rights reserved</p>
+                </div>
+            </div>
+        </div>
---
+++ templates/includes/header.php
@@ -0,0 +1,29 @@
+<header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="info-outer hidden-xs">
+                            <div class="info-box">
+                                <div class="icon"><span class="flaticon-mail"></span></div>
+                                <a href="mailt:companyname@mail.com"><span class="__cf_email__" data-cfemail="ed8e82809d8c8394838c8088ad808c8481c38e8280">[email&#160;protected]</span></a>
+                            </div>
+                            <div class="info-box">
+                                <div class="icon"><span class="flaticon-customer-service"></span></div>
+                                <a class="phone" href="#">(732) 803-010-03</a>
+                            </div>
+                        </div>
+                        <!--Header Social Icon -->
+                        <div class="social">
+                            <ul>
+                                <li><a href="#"><i class="fa fa-lg fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-lg fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-lg fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa fa-lg fa-vimeo-square"></i></a></li>
+                                <li><a href="#"><i class="fa fa-lg fa-dribbble"></i></a></li>
+                                <li><a href="#"><i class="fa fa-lg fa-skype"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
