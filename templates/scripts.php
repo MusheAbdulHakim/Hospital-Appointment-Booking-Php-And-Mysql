1bea34c7c4dc4f615a739453976ff50e6b74f04a
Yahuza Mushe Abdul-Hakim <musheabdulhakim99@gmail.com>
2020-11-09 23:23:48 +0000

    javascript files

---
+++ templates/scripts.php
@@ -0,0 +1,28 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script data-cfasync="false" src="cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="js/jquery.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <script type="aa3a49192a823535c0f0a018-text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVtjo9eO4klWhYbHwL9jObfuke4rxSWWc"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <!-- REVOLUTION JS FILES -->
+        <script src="revolution/js/jquery.themepunch.tools.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <script src="revolution/js/jquery.themepunch.revolution.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <!-- SLIDER REVOLUTION 5.0 EXTENSIONS  
+                (Load Extensions only on Local File Systems ! 
+                 The following part can be removed on Server for On Demand Loading) -->	
+        <script src="revolution/js/extensions/revolution.extension.actions.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <script src="revolution/js/extensions/revolution.extension.carousel.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <script src="revolution/js/extensions/revolution.extension.kenburn.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <script src="revolution/js/extensions/revolution.extension.layeranimation.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <script src="revolution/js/extensions/revolution.extension.migration.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <script src="revolution/js/extensions/revolution.extension.navigation.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <script src="revolution/js/extensions/revolution.extension.parallax.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <script src="revolution/js/extensions/revolution.extension.slideanims.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <!-- owl carousel js -->
+        <script src="owl-carousel/owl.carousel.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <!-- Plugin JavaScript -->
+        <script src="js/jquery.easing.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <!-- Jquery Ui -->
+        <script src="js/jquery-ui.min.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+        <!-- Custom Js -->
+        <script src="js/custom.js" type="aa3a49192a823535c0f0a018-text/javascript"></script>
+    <script src="https://ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="aa3a49192a823535c0f0a018-|49" defer=""></script><script type="text/javascript">(function(){window['__CF$cv$params']={r:'5e7398facc287215',m:'d41e96ed26572afa54b896d2f5cdc4961639102c-1603541457-1800-Ad8FTKsh3xBFEyKjNoduzzQ7Sy978F8lUV/alGGHycqAr2lKliN2+4yx1xThWjOSidEn2ZxHe4d+6pNyXJoPEAPWe8458/Y3nMAqXs43tk/U/1pKvZtT4oUVmpNrJT3C6w==',s:[0xf2e0f28e60,0x0e4b6e1a58],}})();</script>
