888a275f5eb6c911a7a84be7d20b03f968319a14
Yahuza Mushe Abdul-Hakim <musheabdulhakim99@gmail.com>
2020-11-11 22:03:49 +0000

    appointment form

---
+++ templates/appointment_form.php
@@ -0,0 +1,53 @@
+<div class="col-sm-6 col-md-4">
+					<div id="form" class="form-area">
+						<form>
+							<div class="form-padding">
+								<h4>Appointment form</h4>
+								<div class="form-group">
+									<label>NAME AND SURNAME</label>
+									<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+								</div>
+								<div class="form-group">
+									<label>PHONE NUMBER</label>
+									<input type="text" class="form-control" id="email" name="email" placeholder="Email">
+								</div>
+								<div class="form-group">
+									<label>PATIENT NUMBER</label>
+									<input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number">
+								</div>
+								<div class="row">
+									<div class="col-sm-6">
+										<div class="form-group">
+											<label>DATE FORM</label>
+											<input type="text" class="form-control" id="datepicker" name="date-form" placeholder="Form">
+										</div>
+									</div>
+									<div class="col-sm-6">
+										<div class="form-group">
+											<label>DATE To</label>
+											<input type="text" class="form-control" id="datepicker2" name="date_to" placeholder="To">
+										</div>
+									</div>
+								</div>
+								<label>HOURLY PREFERENCE</label>
+								<div class="check-btn" data-toggle="buttons">
+									<label class="btn btn-primary active">
+										<input type="radio" name="options" id="option1" checked> MORNING
+									</label>
+									<label class="btn btn-primary">
+										<input type="radio" name="options" id="option2"> LUNCH
+									</label>
+									<label class="btn btn-primary">
+										<input type="radio" name="options" id="option3"> EVENING
+									</label>
+								</div>
+							</div>
+							<div class="form-footer">
+								<div class="checkbox">
+									<label><input type="checkbox" value="">Send me copy of message</label>
+								</div>
+								<button type="button" id="submit" name="submit" class="btn thm-btn">SUBMIT</button>
+							</div>
+						</form>
+					</div>
+				</div>
