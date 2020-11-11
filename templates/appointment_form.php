382a97b2da5ac8008b9111136de8f5df19d0683b
Yahuza Mushe Abdul-Hakim <musheabdulhakim99@gmail.com>
2020-11-11 23:16:17 +0000

    php backend added

--- templates/appointment_form.php
+++ templates/appointment_form.php
@@ -1,51 +1,62 @@
+<?php 
+	if (isset($_POST['submit'])) {
+		$fullname = htmlspecialchars($_POST['fullname']);
+		$phone = htmlspecialchars($_POST['mobile']);
+		$email = htmlspecialchars($_POST['email']);
+		$from_date = htmlspecialchars($_POST['from_date']);
+		$to_date = htmlspecialchars($_POST['to_date']);
+		$sql = "INSERT into appointments(Name,Phone,Email,From_date,To_date)values(:name,:phone,:email,:from_date,:to_date)";
+		$query = $dbh->prepare($sql);
+		$query->bindParam(':name',$fullname,PDO::PARAM_STR);
+		$query->bindParam(':phone',$phone,PDO::PARAM_STR);
+		$query->bindParam(':email',$email,PDO::PARAM_STR);
+		$query->bindParam(':from_date',$from_date,PDO::PARAM_STR);
+		$query->bindParam(':to_date',$to_date,PDO::PARAM_STR);
+		$query->execute();
+		$lastinsertid = $dbh->LastInsertId();
+		if ($lastinsertid>0) {
+			echo "<script>alert('You appointment has been sent');</script>";
+		}else{
+			cho "<script>alert('Something went wrong.please try again.');</script>";
+		}
+
+	}
+
+ ?>
 <div class="col-sm-6 col-md-4">
 					<div id="form" class="form-area">
-						<form>
+						<form method="POST">
 							<div class="form-padding">
 								<h4>Appointment form</h4>
 								<div class="form-group">
-									<label>NAME AND SURNAME</label>
-									<input type="text" class="form-control" id="name" name="name" placeholder="Name">
+									<label>FULL NAME</label>
+									<input type="text" class="form-control" id="name" name="fullname" placeholder="FULL Name">
 								</div>
 								<div class="form-group">
 									<label>PHONE NUMBER</label>
-									<input type="text" class="form-control" id="email" name="email" placeholder="Email">
+									<input type="tel" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number">
 								</div>
 								<div class="form-group">
-									<label>PATIENT NUMBER</label>
-									<input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number">
+									<label>EMAIL ADDRESS</label>
+									<input type="email" class="form-control" id="email" name="email" placeholder="Email Address">
 								</div>
 								<div class="row">
 									<div class="col-sm-6">
 										<div class="form-group">
-											<label>DATE FORM</label>
-											<input type="text" class="form-control" id="datepicker" name="date-form" placeholder="Form">
+											<label>FROM</label>
+											<input type="text" class="form-control" value="<?php echo date('d-M-yy');?>" id="datepicker" name="from_date">
 										</div>
 									</div>
 									<div class="col-sm-6">
 										<div class="form-group">
-											<label>DATE To</label>
-											<input type="text" class="form-control" id="datepicker2" name="date_to" placeholder="To">
+											<label>TO DATE</label>
+											<input type="text" class="form-control" id="datepicker2" name="to_date" placeholder="To">
 										</div>
 									</div>
 								</div>
-								<label>HOURLY PREFERENCE</label>
-								<div class="check-btn" data-toggle="buttons">
-									<label class="btn btn-primary active">
-										<input type="radio" name="options" id="option1" checked> MORNING
-									</label>
-									<label class="btn btn-primary">
-										<input type="radio" name="options" id="option2"> LUNCH
-									</label>
-									<label class="btn btn-primary">
-										<input type="radio" name="options" id="option3"> EVENING
-									</label>
-								</div>
+								
 							</div>
 							<div class="form-footer">
-								<div class="checkbox">
-									<label><input type="checkbox" value="">Send me copy of message</label>
-								</div>
 								<button type="button" id="submit" name="submit" class="btn thm-btn">SUBMIT</button>
 							</div>
 						</form>
